d7c475801466b4addb4e463a390ab9c460a29188
shohrux508 <yigitalievshohrux@gmail.com>
2023-12-29 10:33:20 +0500

    i have added search functions to models: Authors and Books

--- app1/urls.py
+++ app1/urls.py
@@ -3,9 +3,15 @@ from . import views
 
 urlpatterns = [
     path('api/v1/authors', views.AuthorsListViewAPI.as_view()),
+    path('api/v1/authors/new', views.AuthorsCreateViewAPI.as_view()),
     path('api/v1/authors/<int:pk>/', views.AuthorsRetrieveUpdateDestroyAPIView.as_view()),
     path('api/v1/books', views.BooksListViewAPI.as_view()),
+    path('api/v1/books/new', views.BooksCreateViewAPI.as_view()),
     path('api/v1/books/<int:pk>/', views.BookRetrieveUpdateDestroyAPIView.as_view()),
+    path('api/v1/authors/search/', views.AuthorsSearchViewAPI.as_view()),
+    path('api/v1/books/search/', views.BooksSearchViewAPI.as_view()),
+
+
 
 
 ]
--- app1/views.py
+++ app1/views.py
@@ -1,11 +1,23 @@
+import rest_framework.views
 from django.shortcuts import render
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+
 from . import models
 from . import serializers
 
 
 # Create your views here.
+
+class AuthorsCreateViewAPI(generics.CreateAPIView):
+    queryset = models.Author.objects.all()
+    serializer_class = serializers.AuthorSerializer
+
+
+class BooksCreateViewAPI(generics.CreateAPIView):
+    queryset = models.Book.objects.all()
+    serializer_class = serializers.AuthorSerializer()
 
 
 class AuthorsListViewAPI(generics.ListAPIView):
@@ -30,7 +42,33 @@ class BookRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = (IsAuthenticated,)
 
 
-def welcome(request):
-    print(request)
-    print("Hello World")
+# search
 
+class AuthorsSearchViewAPI(generics.ListAPIView):
+    serializer_class = serializers.AuthorSerializer
+
+    def get_queryset(self):
+        gender = self.request.query_params.get('gender')
+        bio = self.request.query_params.get('bio')
+        age = self.request.query_params.get('age')
+        name = self.request.query_params.get('name')
+        queryset = models.Author.objects.all()
+        if gender:
+            queryset = queryset.filter(gender=gender)
+        elif bio:
+            queryset = queryset.filter(bio=bio)
+        elif age:
+            queryset = queryset.filter(age=age)
+        elif name:
+            queryset = queryset.filter(name=name)
+        return queryset
+
+class BooksSearchViewAPI(generics.ListAPIView):
+    serializer_class = serializers.BookSerializer
+
+    def get_queryset(self):
+        title = self.request.query_params.get('title')
+        queryset = models.Book.objects.all()
+        if title:
+            queryset = queryset.filter(title=title)
+        return queryset
